384ab4cc8d4e74716de008d53db1c40d36a0a6c5
sean-cunniffe <sean.cunniffe927@gmail.com>
2021-02-06 14:10:20 +0000

    Initial commit basic functionality, generates graphs from csv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Log} from './common/log';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'workout-csv-grapher';
+  logs: Log[] = [];
+  logMapByExerciseName: Map<string, Log[]> = new Map<string, Log[]>();
+  public graph: any = [];
+
+  getFile(event: Event): void {
+    const fileList: FileList = (event.target as HTMLInputElement).files;
+    const file = fileList.item(0);
+    const fileReader = new FileReader();
+    fileReader.onload = ev => {
+      const split1 = (fileReader.result as string).replace(new RegExp('"', 'g'), '').split('\n');
+      for (const set of split1) {
+        const setSplit = set.split(';');
+        const log = new Log(new Date(setSplit[0]), setSplit[1], setSplit[2], +setSplit[3], +setSplit[4], setSplit[5]
+          , +setSplit[6], +setSplit[7], +setSplit[8], setSplit[9], +setSplit[10], setSplit[11], setSplit[12]);
+        this.logs.push(log);
+      }
+      this.logs.splice(0, 1);
+      this.logs.splice(this.logs.length - 1, 1);
+
+      this.generateGraphsByVolumePerDay();
+    };
+    fileReader.readAsText(file);
+  }
+
+  splitIntoExerciseNames(logs: Log[]): Map<string, Log[]> {
+    const sets: Map<string, Log[]> = new Map<string, Log[]>();
+    for (const log of logs) {
+      if (sets.has(log.exerciseName)) {
+        sets.get(log.exerciseName).push(log);
+      } else {
+        sets.set(log.exerciseName, [log]);
+      }
+    }
+    sets.delete('Workout Name');
+    return sets;
+  }
+
+
+  generateGraphsByVolumePerDay(): void {
+    const sets: Map<string, Log[]> = this.splitIntoExerciseNames(this.logs);
+    sets.forEach(((value, key) => this.getByExerciseByVolumePerDay(key, value)));
+  }
+
+
+  private getByExerciseByVolumePerDay(exerciseName: string, sets: Log[]): void {
+    const mappingByDate: Map<number, Log[]> = new Map<number, Log[]>();
+
+    // map by date
+    sets.forEach(
+      (set, key) => mappingByDate.has(+set.date) ? mappingByDate.get(+set.date).push(set) : mappingByDate.set(+set.date, [set])
+    );
+    // add all values together from each date
+    const yValues: number[] = [];
+    const xValues: Date[] = [];
+    mappingByDate.forEach((logs, key) => {
+      xValues.push(new Date(key));
+      yValues.push(logs.reduce((previousValue, currentValue) => previousValue + (currentValue.reps * currentValue.weight), 0));
+    });
+    this.graph.push({
+      data: [{x: xValues, y: yValues, type: 'scatter', marker: {color: 'red'}},
+      ],
+      layout: {width: window.innerWidth, title: exerciseName},
+      sets: mappingByDate
+    });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import * as PlotlyJS from 'plotly.js/dist/plotly.js';
+import { PlotlyModule } from 'angular-plotly.js';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+PlotlyModule.plotlyjs = PlotlyJS;
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PlotlyModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
