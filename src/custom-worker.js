bb37eea6e0f005ab081c4173306d257c53d6af07
sean-cunniffe <sean.cunniffe927@gmail.com>
2021-03-17 11:05:13 +0000

    Added print out to all events to see which is fired for the file share event

--- src/custom-worker.js
+++ src/custom-worker.js
@@ -1,18 +1,30 @@
 importScripts('./ngsw-worker.js');
+// self.addEventListener('fetch', event => {
+//   console.log('hello from worker');
+//   // If this is an incoming POST request for the
+//   // registered "action" URL, respond to it.
+//   if (event.request.method === 'POST') {
+//     event.respondWith((async () => {
+//       const formData = await event.request.file;
+//       const files = formData.get('files') || [];
+//       postMessage(files[0])
+//     }));
+//   } else {
+//     return Response.redirect('/hello', 303);
+//   }
+// });
 self.addEventListener('fetch', event => {
-  console.log('hello from worker');
-  // If this is an incoming POST request for the
-  // registered "action" URL, respond to it.
-  if (event.request.method === 'POST') {
-    event.respondWith((async () => {
-      const formData = await event.request.file;
-      const files = formData.get('files') || [];
-      postMessage(files[0])
-    }));
-  } else {
-    return Response.redirect('/hello', 303);
-  }
-});
+  console.log('fetch event');
+  console.log(event);
+})
+self.addEventListener('sync', event => {
+  console.log('sync event');
+  console.log(event);
+})
+self.addEventListener('push', event => {
+  console.log('push event');
+  console.log(event);
+})
 // function getBase64(file) {
 //   return new Promise((resolve, reject) => {
 //     const reader = new FileReader();
